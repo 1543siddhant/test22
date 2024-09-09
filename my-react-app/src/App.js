58a2d8c90b5cae1bbfb39e0787919dabe0f20057
1543siddhant <siddhantpatil1543@gmail.com>
2024-09-09 15:19:16 +0530

    2

---
+++ my-react-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';  // Optional, for adding styles
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My React App!?????</h1>
+        <p>This is a simple React application Hahajaja.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
